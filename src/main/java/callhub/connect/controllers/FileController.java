94b6ad116205a9e70cf42f433bb0271c48adba7c
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-05 14:27:21 -0500

    upload PDF through postman to MongoDB

--- src/main/java/callhub/connect/controllers/FileController.java
+++ src/main/java/callhub/connect/controllers/FileController.java
@@ -1,10 +1,13 @@
 package callhub.connect.controllers;
+import callhub.connect.data_access.DataAccess;
 import callhub.connect.data_access.DocumentRepository;
 import callhub.connect.data_access.LocalDataAccess;
+import callhub.connect.data_access.NetworkDataAccess;
 import callhub.connect.entities.FileDocument;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -14,17 +17,34 @@ import java.time.LocalDate;
 public class FileController {
 
     @Autowired
-    DocumentRepository documentRepository;
-    @GetMapping("/upload")
-    public String uploadPDF () {
+    public DocumentRepository documentRepository;
+    DataAccess dataAccessObject;
+    @GetMapping("/upload_local")
+    public String uploadPDFLocal () {
         String currentDirectory = System.getProperty("user.dir");
         String pathName = currentDirectory + "/src/main/java/callhub/connect/pdfs/fall2023.pdf";
         FileDocument result;
+
+        dataAccessObject = new LocalDataAccess(pathName);
+        Binary data;
         try {
-            byte[] pdfEncoded = LocalDataAccess.convertPDFToByteArray(pathName);
-            Binary data = LocalDataAccess.byteArrayToBinary(pdfEncoded);
+            data = dataAccessObject.serializePDF();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        result = documentRepository.save(new FileDocument("timetable", data, LocalDate.now()));
+        return "Uploaded! " + result.id;
+    }
+
+    @PostMapping("/upload_network")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        dataAccessObject = new NetworkDataAccess(file);
+        FileDocument result;
+        Binary data;
+        try {
+            data = dataAccessObject.serializePDF();
             result = documentRepository.save(new FileDocument("timetable", data, LocalDate.now()));
-            return "Uploaded! " + result.id;
+            return result.id;
         } catch (IOException e) {
             return e.getMessage();
         }
--- src/main/java/callhub/connect/data_access/LocalDataAccess.java
+++ src/main/java/callhub/connect/data_access/LocalDataAccess.java
@@ -5,27 +5,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import org.bson.types.Binary;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class LocalDataAccess {
-    public static byte[] convertPDFToByteArray(String filePath) throws IOException {
+public class LocalDataAccess extends DataAccess {
+    private String pathName;
+    public LocalDataAccess(String pathName) {
+        this.pathName = pathName;
+
+    }
+    private static byte[] convertPDFToByteArrayUsingPath(String filePath) throws IOException {
         File pdfFile = new File(filePath);
-        // 1 mb = 1024 * 1024
-
-        try (FileInputStream fileInputStream = new FileInputStream(pdfFile)) {
-            if (pdfFile.length() > 16 * 1024 * 1024) {
-                throw new FileLimitExceededException("PDF document exceeds 16MB.");
-            }
-
-            byte[] pdfBytes = new byte[(int) pdfFile.length()];
-            int bytesRead = fileInputStream.read(pdfBytes);
-            if (bytesRead < 0) {
-                throw new IOException("Failed to read PDF file");
-            }
-            return pdfBytes;
-        }
+        return getPDFBytes(pdfFile);
     }
 
-    public static Binary byteArrayToBinary(byte[] byteArray) {
-        return new Binary(byteArray);
+
+    @Override
+    public Binary serializePDF() throws IOException {
+        return byteArrayToBinary(convertPDFToByteArrayUsingPath(this.pathName));
     }
 }
