ca5473d8ebe1146b2a553416fd0a26ad210da789
zjayee <jayee.zheng@gmail.com>
2023-11-10 02:00:37 -0500

    Setup 2 way websocket connection

--- src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
+++ src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
@@ -4,34 +4,21 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-//@Controller
+@Controller
 public class MessageWebSocketAccess {
-    String sessionId;
-    String sessionCode;
-
-    public MessageWebSocketAccess(String sessionId, String sessionCode) {
-        this.sessionId = sessionId;
-        this.sessionCode = sessionCode;
-    }
 
     @MessageMapping("/message-customer/{sessionId}")
     @SendTo("/topic/message-employee/{sessionId}")
     public String sendMessageCustomer(@DestinationVariable String sessionId, String message) throws Exception {
-        System.out.println("message");
+        System.out.println(message);
         return message + " (" + sessionId + ")";
     }
 
     @MessageMapping("/message-employee/{sessionId}")
     @SendTo("/topic/message-customer/{sessionId}")
     public String sendMessageEmployee(@DestinationVariable String sessionId, String message) throws Exception {
-        System.out.println("message");
+        System.out.println(message);
         return message + " (" + sessionId + ")";
     }
 
-    @MessageMapping("/message")
-    @SendTo("/topic/messages")
-    public String sendMessage(String message) throws Exception {
-        System.out.println("message");
-        return message + " received";
-    }
 }
