71d6823e299e66e1c8bc7758899d2539ee5a8fba
zjayee <jayee.zheng@gmail.com>
2023-11-21 00:55:19 -0500

    Return messages as json objects

--- src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
+++ src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
@@ -1,24 +1,36 @@
 package callhub.connect.drivers;
+import com.google.gson.Gson;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
+
 @Controller
 public class MessageWebSocketAccess {
 
+    private static final Gson gson = new Gson();
+
     @MessageMapping("/message-customer/{sessionId}")
     @SendTo("/topic/message-employee/{sessionId}")
-    public String sendMessageCustomer(@DestinationVariable String sessionId, String message) throws Exception {
+    public String sendMessageCustomer(@DestinationVariable String sessionId, String message, String timestamp) throws Exception {
         System.out.println(message);
-        return message + " (" + sessionId + ")";
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", message);
+        response.put("timestamp", timestamp);
+
+        return gson.toJson(response);
     }
 
     @MessageMapping("/message-employee/{sessionId}")
     @SendTo("/topic/message-customer/{sessionId}")
-    public String sendMessageEmployee(@DestinationVariable String sessionId, String message) throws Exception {
+    public String sendMessageEmployee(@DestinationVariable String sessionId, String message, String timestamp) throws Exception {
         System.out.println(message);
-        return message + " (" + sessionId + ")";
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", message);
+        response.put("timestamp", timestamp);
+        return gson.toJson(response);
     }
 
 }
