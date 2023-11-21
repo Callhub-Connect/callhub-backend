7d966a41aa8a119c041aacc85c627671eeb590c8
zjayee <jayee.zheng@gmail.com>
2023-11-21 01:32:38 -0500

    Add timestamp from current time

--- src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
+++ src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
@@ -5,31 +5,39 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Locale;
 
 @Controller
 public class MessageWebSocketAccess {
 
     private static final Gson gson = new Gson();
+    final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss a", Locale.CANADA);
 
     @MessageMapping("/message-customer/{sessionId}")
     @SendTo("/topic/message-employee/{sessionId}")
-    public String sendMessageCustomer(@DestinationVariable String sessionId, String message, String timestamp) throws Exception {
+    public String sendMessageCustomer(@DestinationVariable String sessionId, String message) throws Exception {
         System.out.println(message);
         HashMap<String, String> response = new HashMap<>();
-        response.put("message", message);
-        response.put("timestamp", timestamp);
+        LocalDateTime timestamp = LocalDateTime.now();
 
+        response.put("message", message);
+        response.put("timestamp", timestamp.format(TIME_FORMATTER).replace("a.m.", "AM").replace("p.m.","PM"));
+
+        System.out.println(gson.toJson(response));
         return gson.toJson(response);
     }
 
     @MessageMapping("/message-employee/{sessionId}")
     @SendTo("/topic/message-customer/{sessionId}")
-    public String sendMessageEmployee(@DestinationVariable String sessionId, String message, String timestamp) throws Exception {
-        System.out.println(message);
+    public String sendMessageEmployee(@DestinationVariable String sessionId, String message) throws Exception {
         HashMap<String, String> response = new HashMap<>();
+        LocalDateTime timestamp = LocalDateTime.now();
+
         response.put("message", message);
-        response.put("timestamp", timestamp);
+        response.put("timestamp", timestamp.format(TIME_FORMATTER));
         return gson.toJson(response);
     }
 
