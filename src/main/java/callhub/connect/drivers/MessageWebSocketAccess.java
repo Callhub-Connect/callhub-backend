b1300c6d92a4a4eed00e0fd18caf50fe280bbda6
zjayee <jayee.zheng@gmail.com>
2023-11-21 22:58:24 -0500

    Moved generate response to interactor

--- src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
+++ src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
@@ -1,12 +1,12 @@
 package callhub.connect.drivers;
+import callhub.connect.use_case.MessageInteractor;
 import com.google.gson.Gson;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.HashMap;
 import java.util.Locale;
 
@@ -14,30 +14,21 @@ import java.util.Locale;
 public class MessageWebSocketAccess {
 
     private static final Gson gson = new Gson();
-    final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss a", Locale.CANADA);
+    private MessageInteractor messageInteractor = new MessageInteractor();
 
     @MessageMapping("/message-customer/{sessionId}")
     @SendTo("/topic/message-employee/{sessionId}")
     public String sendMessageCustomer(@DestinationVariable String sessionId, String message) throws Exception {
-        HashMap<String, String> response = generateResponse(message);
+        HashMap<String, String> response = messageInteractor.generateResponse(message);
         return gson.toJson(response);
     }
 
     @MessageMapping("/message-employee/{sessionId}")
     @SendTo("/topic/message-customer/{sessionId}")
     public String sendMessageEmployee(@DestinationVariable String sessionId, String message) throws Exception {
-        HashMap<String, String> response = generateResponse(message);
+        HashMap<String, String> response = messageInteractor.generateResponse(message);
         return gson.toJson(response);
     }
 
-    private HashMap<String, String> generateResponse(String message){
-        HashMap<String, String> response = new HashMap<>();
-        LocalDateTime timestamp = LocalDateTime.now();
-
-        response.put("message", message);
-        response.put("timestamp", timestamp.format(TIME_FORMATTER).replace("a.m.", "AM").replace("p.m.","PM"));
-
-        return response;
-    }
 
 }
