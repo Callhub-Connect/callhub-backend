42f5c6cc8075f8ada690d5da8aab627b18473ea0
vithushon <v.thayalan@mail.utoronto.ca>
2023-11-26 22:21:36 -0500

    Attempting to resolve merge conflict

---
+++ src/main/java/callhub/connect/drivers/MessageWebSocketAccess.java
@@ -0,0 +1,6 @@
+package callhub.connect.drivers;
+
+public class MessageWebSocketAccess {
+    // this class was moved to MessageController
+    // Attempting to see if this resolves the merge conflict
+}
